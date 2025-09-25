25d7e9a3649d6d7a6656e7f3d6c354428dc19233
sinueh-andreata <sinuehnoan@gmail.com>
2025-09-25 15:37:15 -0300

    security extensions

--- src/extensions.py
+++ src/extensions.py
@@ -2,9 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 
 db = SQLAlchemy()
 
 csrf = CSRFProtect()
+
+security = Security()
 
 limiter = Limiter(key_func=get_remote_address, default_limits=["200 per day", "50 per hour"])
