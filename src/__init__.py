c4083751717162ed875adfdf840053c8eba7dcec
sinueh-andreata <sinuehnoan@gmail.com>
2025-09-25 15:46:19 -0300

    __init__.py atualizado

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from .config import ConfigDev
-from .extensions import db, csrf, limiter
+from .extensions import db, csrf, limiter, security
+from .models.models import User, Role
+from flask_security import SQLAlchemyUserDatastore
 
 def create_app(config_class=ConfigDev):
     app = Flask(__name__)
@@ -11,6 +13,10 @@ def create_app(config_class=ConfigDev):
     db.init_app(app)
     csrf.init_app(app)
     limiter.init_app(app)
+
+    # configuração do Flask-Security
+    user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+    security.init_app(app, user_datastore)  
 
     @app.route("/")
     def index():
