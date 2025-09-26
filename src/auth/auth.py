849cb3132c4fe1a1bc432bd008a213a660392593
sinueh-andreata <sinuehnoan@gmail.com>
2025-09-26 11:15:21 -0300

    csrf token to frontend

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, render_template, redirect, request, url_for
 from flask_security import user_registered
+from ..extensions import csrf
 from ..extensions import db
 from ..models.models import Role
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/csrf-token', methods=['GET'])
+def get_csrf_token():
+    token = csrf.generate_csrf()
+    return {'csrf_token': token}
 
 def on_user_registered(sender, user):
     default_role = Role.query.filter_by(name="usuario").first()
@@ -11,3 +19,4 @@ def on_user_registered(sender, user):
 
 def init_app(app):
     user_registered.connect(on_user_registered, app)
+    app.register_blueprint(auth_bp)
