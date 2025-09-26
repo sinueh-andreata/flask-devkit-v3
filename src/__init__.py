deeb7eb2d77c5582c256a905f73b59c8eaf78973
sinueh-andreata <sinuehnoan@gmail.com>
2025-09-26 14:09:34 -0300

    MEUS TEMPLATES FORAM

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, app, render_template, redirect, url_for
 from .config import ConfigDev
 from .extensions import db, csrf, limiter, security
 from .models.models import User, Role
 from flask_security import SQLAlchemyUserDatastore
+from flask_security.forms import LoginForm, RegisterForm
+from flask_security.utils import verify_and_update_password
 from src.auth.datastore import user_datastore  
 from src.auth import init_app as init_auth
 from src.usuario.usuarios import usuarios_bp
@@ -21,10 +23,7 @@ def create_app(config_class=ConfigDev):
     limiter.init_app(app)
 
     # configuração do Flask-Security
-    security.init_app(app, user_datastore,
-                    register_form=None,
-                    login_form=None
-                    )
+    security.init_app(app, user_datastore)
 
     # registra os blueprints
     app.register_blueprint(usuarios_bp)
@@ -36,12 +35,7 @@ def create_app(config_class=ConfigDev):
     # Cria todas as tabelas do banco de dados automaticamente
     with app.app_context():
         db.create_all()
-        
-        if not Role.query.filter_by(name='usuario').first():
-            usuario_role = Role(name='usuario', description='Usuário padrão')
-            db.session.add(usuario_role)
-        db.session.commit()
-    
+
     @app.errorhandler(404)
     def page_not_found(e):
         return render_template('404.html'), 404
@@ -53,5 +47,21 @@ def create_app(config_class=ConfigDev):
     @app.route("/")
     def index():
         return render_template('index.html')
+    
+    @app.route('/entrar', methods=['GET', 'POST'])
+    def entrar():
+        form = LoginForm()
+        if form.validate_on_submit():
+
+            pass
+        return render_template('security/login.html', form=form)
+
+    @app.route('/cadastrar', methods=['GET', 'POST'])
+    def cadastrar():
+        form = RegisterForm()
+        if form.validate_on_submit():
+            # criar usuário manualmente ou usar user_datastore.create_user
+            pass
+        return render_template('security/register.html', form=form)
 
     return app
