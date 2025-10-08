4cbb61a999525646aa7505d81ce69c0fd59fee56
sinueh-andreata <sinuehnoan@gmail.com>
2025-10-08 09:02:26 -0300

    update: addusers

--- add-users.py
+++ add-users.py
@@ -1,46 +1,52 @@
-# Script para adicionar usuários e roles ao banco de dados
-# Para descomentar tudo use Ctrl + ; ou Ctrl + Shift + K + U
-# Ou simplemente delete esse arquivo
+# Script para adicionar usuários e roles padrão ao banco de dados
 
-# from src import create_app
-# from src.extensions import db
-# from src.auth.datastore import admin_datastore, user_datastore
+from src import admin, create_app
+from src.extensions import db
+from src.auth.datastore import user_datastore
+from argon2 import PasswordHasher
 
-# app = create_app()
+ph = PasswordHasher()
 
-# with app.app_context():
-#     if not admin_datastore.find_role("admin"):
-#         admin_datastore.create_role(name="admin", description="Administrador")
-#         db.session.commit()
+def hash_password(password: str) -> str:
+    return ph.hash(password)
 
-#     email = "admin@admin.com"
-#     password = "senha-123"
-#     if not admin_datastore.find_user(email=email):
-#         admin = admin_datastore.create_user(email=email, password=password, active=True)
-#         db.session.commit()
-#     else:
-#         admin = admin_datastore.find_user(email=email)
+app = create_app()
 
-#     if "admin" not in [role.name for role in admin.roles]:
-#         admin_datastore.add_role_to_user(admin, "admin")
-#         db.session.commit()
+with app.app_context():
+    if not user_datastore.find_role("admin"):
+        user_datastore.create_role(name="admin", description="Administrador")
+        db.session.commit()
 
-#     print("Admin criado e role atribuída!")
+    email = "adminrs@admin.com"
+    password = "senha-1234"
+    hashed_password = hash_password(password)
+    if not user_datastore.find_user(email=email):
+        user = user_datastore.create_user(email=email, password=hashed_password, active=True)
+        db.session.commit()
+    else:
+        user = user_datastore.find_user(email=email)
 
-#     if not user_datastore.find_role("user"):
-#         user_datastore.create_role(name="user", description="Usuário padrão")
-#         db.session.commit()
+    if "admin" not in [role.name for role in user.roles]:
+        user_datastore.add_role_to_user(user, "admin")
+        db.session.commit()
 
-#     email = "user@user.com"
-#     password = "senha-123"
-#     if not user_datastore.find_user(email=email):
-#         user = user_datastore.create_user(email=email, password=password, active=True)
-#         db.session.commit()
-#     else:
-#         user = user_datastore.find_user(email=email)
+    print("Admin criado e role atribuída!")
 
-#     if "user" not in [role.name for role in user.roles]:
-#         user_datastore.add_role_to_user(user, "user")
-#         db.session.commit()
+    if not user_datastore.find_role("user"):
+        user_datastore.create_role(name="user", description="Usuário padrão")
+        db.session.commit()
 
-#     print("Usuário criado e role atribuída!")
+    email = "userrs@user.com"
+    password = "senha-1234"
+    hashed_password = hash_password(password)
+    if not user_datastore.find_user(email=email):
+        user = user_datastore.create_user(email=email, password=hashed_password, active=True)
+        db.session.commit()
+    else:
+        user = user_datastore.find_user(email=email)
+
+    if "user" not in [role.name for role in user.roles]:
+        user_datastore.add_role_to_user(user, "user")
+        db.session.commit()
+
+    print("Usuário criado e role atribuída!")
