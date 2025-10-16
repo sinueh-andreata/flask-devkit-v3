aaf51ed548cc77a1b30c4bd3d7759b59281a3b1f
sinueh-andreata <sinuehnoan@gmail.com>
2025-10-16 09:40:07 -0300

    feat: comentarios

--- src/auth/datastore.py
+++ src/auth/datastore.py
@@ -2,4 +2,5 @@ from flask_security import SQLAlchemyUserDatastore
 from ..extensions import db
 from ..models.models import User, Role
 
+# define o user_datastore para ser usado na configuração do Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
