37d1ff05e786fe1e7ed52924c6ade967551286e3
sinueh-andreata <sinuehnoan@gmail.com>
2025-10-20 17:43:46 -0300

    feat: root homepage

--- src/root/root.py
+++ src/root/root.py
@@ -1,8 +1,8 @@
 from flask_security import login_required
-from flask import Blueprint, jsonify, request, redirect, url_for
+from flask import Blueprint, jsonify, render_template, request, redirect, url_for
 from flask_security.decorators import roles_required, current_user
 
-root_bp = Blueprint('root', __name__)
+root_bp = Blueprint('root', __name__, url_prefix='/root')
 
 @root_bp.route("/rtroot")
 @login_required
@@ -11,4 +11,13 @@ def minha_rota():
     if not current_user.has_role('root'):
         return jsonify({"message": "Acesso negado: você não tem a role necessária"}), 403
     return jsonify({"message": "Apenas root autenticados veem isso"})
+
+@root_bp.route("/")
+@login_required
+@roles_required('root') 
+def root_home():
+    return render_template('root_templates/home.html')
+
+
+
 
