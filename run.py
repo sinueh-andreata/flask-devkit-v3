9e62642ffe1cf708028272c46ab4be930386af3e
sinueh-andreata <sinuehnoan@gmail.com>
2025-10-20 17:17:47 -0300

    chore: app enviroment ports and debugs refactored

--- run.py
+++ run.py
@@ -4,4 +4,11 @@ from app import app
 if __name__ == '__main__':
     os.environ['FLASK_ENV'] = 'development'
 
-    app.run(host='localhost', port=5000, debug=True, use_reloader=True, use_debugger=True)
+    print("running in http://localhost:5555")
+    app.run(host='localhost', port=5555, debug=True, use_reloader=True, use_debugger=True)
+
+if __name__ == '__main__':
+    os.environ['FLASK_ENV'] = 'production'
+    
+    print("running in http://localhost:5777")
+    app.run(host='0.0.0.0', port=5777)
